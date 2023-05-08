ed3689963f82cbdc9bbb456f1170c022848f1d15
angel.garcia <angel.garcia@students.salle.url.edu>
2023-05-08 14:51:02 +0200

    Structured and started profile section

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace Salle\PuzzleMania\Controller;
 
+use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -16,12 +17,18 @@ class ProfileController
 
     public function showProfile(Request $request, Response $response): Response {
         $username = '';
-        if(isset($_SESSION['user'])) {
-            $username = substr($_SESSION['user'], 0, strpos($_SESSION['user'], '@'));
+        $formData = [];
+        $formAction = RouteContext::fromRequest($request)->getRouteParser()->urlFor('signIn');
+        if(isset($_SESSION['user_email'])) {
+            $username = substr($_SESSION['user_email'], 0, strpos($_SESSION['user_email'], '@'));
+            $formData['email'] = $_SESSION['user_email'];
         }
 
         return $this->twig->render($response, 'profile.twig', [
-            'username' => $username
+            'username' => $username,
+            'formAction' => $formAction,
+            'formData' => $formData,
+            'uploads' => "../public/uploads/"
         ]);
     }
 
